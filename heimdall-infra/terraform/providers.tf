a2915d767cb88c02240397169a1bd8e20789ef4b
Dawid Imbrzykowski <dawid.imb@gmail.com>
2025-09-03 20:16:01 +0200

    fixed providers.tf

--- heimdall-infra/terraform/providers.tf
+++ heimdall-infra/terraform/providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-provider "proxmox" {
+proxmox = {
     pm_api_url = var.proxmox.api_url
     pm_api_token_id = var.proxmox.api_token_id
     pm_api_token_secret = var.proxmox.api_token_secret
