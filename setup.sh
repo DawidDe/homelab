d53fef9e8154e33cc4f7cc28a8b2f8eee048f361
Dawid Imb <dawid.imb@gmail.com>
2025-09-15 15:35:20 +0200

    fixed setup.sh and bootstrap.yaml

--- setup.sh
+++ setup.sh
@@ -7,18 +7,18 @@ name=heimdall
 
 # 1. Bootstrap servers
 ansible-playbook ./base/ansible/playbooks/bootstrap-server.yaml \
-    -e vault_address=${vault_token} \
+    -e vault_address=${vault_address} \
     -e vault_token=${vault_token} \
     -e name=${name}
 
 # 2. Bootstrap proxmox
 ansible-playbook ./base/ansible/playbooks/bootstrap-proxmox.yaml \
-    -e vault_address=${vault_token} \
+    -e vault_address=${vault_address} \
     -e vault_token=${vault_token} \
     -e name=${name}
 
 # 3. Terraform
 terraform apply --auto-approve \
-    -e vault_address=${vault_token} \
+    -e vault_address=${vault_address} \
     -var="VAULT_TOKEN=${vault_token}" \
     -var="name=${name}"
