121285f6acba7db603c5aa73f08f0f21a3e65915
Dawid Imb <dawid.imb@gmail.com>
2025-09-18 17:47:00 +0200

    fixed url for proxmox in terraform

--- base/terraform/main.tf
+++ base/terraform/main.tf
@@ -1,5 +1,5 @@
 provider "proxmox" {
-    pm_api_url = "https://${data.vault_kv_secret_v2.secrets.data["proxmox_ip"]}/api2/json"
+    pm_api_url = "https://${data.vault_kv_secret_v2.secrets.data["proxmox_ip"]}:8006/api2/json"
     pm_api_token_id = data.vault_kv_secret_v2.secrets.data["proxmox_api_token_id"]
     pm_api_token_secret = data.vault_kv_secret_v2.secrets.data["proxmox_api_token_secret"]
     pm_tls_insecure = true
