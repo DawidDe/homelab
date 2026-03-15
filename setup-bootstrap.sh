c5cce89e829f897607f2036264a46e002f838e4f
Dawid Imbrzykowski <dawidimb@proton.me>
2026-03-15 20:01:14 +0100

    added more setup steps to bootstrap. added automatic deployment of other resources for external secrets

--- setup-bootstrap.sh
+++ setup-bootstrap.sh
@@ -14,3 +14,20 @@ talosctl bootstrap --nodes $CONTROL_PLANE_IP --talosconfig=./talosconfig
 sleep 60
 
 talosctl kubeconfig --nodes $CONTROL_PLANE_IP --talosconfig=./talosconfig
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml --server-side --force-conflicts
+
+kubectl apply -f base/kubernetes/bootstrap/external-secrets-operator/app.yaml
+
+kubectl apply -f base/kubernetes/external-secrets-operator/app.yaml
+encoded_vault_token=$(echo -n "$vault_token" | base64)
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: vault-token
+  namespace: external-secrets-operator
+data:
+  token: ${encoded_vault_token}
+EOF
