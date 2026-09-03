74e7bb5ce8be4f1b9f0aecc90e47c3bef591727c
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-03 19:27:11 +0200

    fixed syntax errors. made dawid systemuser

--- nixos/modules/services/firewall.nix
+++ nixos/modules/services/firewall.nix
@@ -8,5 +8,5 @@
       # Allow SSH only from 192.168.178.67
       ip saddr 192.168.178.67 tcp dport 22 accept
     '';
-  }
+  };
 }
--- nixos/modules/system/users.nix
+++ nixos/modules/system/users.nix
@@ -9,10 +9,11 @@
       ];
       group = "users";
       home = "/home/dawid";
+      isSystemUser = true;
 
       openssh.authorizedKeys.keys = [
           "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBKzkzCs5JnFDhBB7YAmnzcd0S6tbMsV1XGJ3B4aod+TdJUI0ngLLyFQxJVpXivExqqHUtPH14HdpV5qWVx5NnV4AAAALdGVybWl1cy5jb20="
       ];
-    }
-  }
+    };
+  };
 }
