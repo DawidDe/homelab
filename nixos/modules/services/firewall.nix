ef89da9c60feda43d9feacb5d08463e2060f3fd4
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-04 19:39:54 +0200

    fixed rule

--- nixos/modules/services/firewall.nix
+++ nixos/modules/services/firewall.nix
@@ -7,7 +7,7 @@
 
     extraInputRules = ''
       # Allow SSH only from 192.168.178.67
-      iif "eno1" ip saddr 192.168.178.67/32 tcp dport 22 accept
+      iifname "eno1" ip saddr 192.168.178.67 tcp dport 22 accept
     '';
   };
 }
