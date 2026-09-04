29768dc3f31eaecb9248b509ca21c194ec623e07
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-04 18:39:47 +0200

    fixed shell

--- nixos/modules/system/users.nix
+++ nixos/modules/system/users.nix
@@ -10,7 +10,7 @@
       group = "users";
       home = "/home/dawid";
       isSystemUser = true;
-      shell = pkgs.sudo;
+      shell = pkgs.bash;
 
       openssh.authorizedKeys.keys = [
           "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBKzkzCs5JnFDhBB7YAmnzcd0S6tbMsV1XGJ3B4aod+TdJUI0ngLLyFQxJVpXivExqqHUtPH14HdpV5qWVx5NnV4AAAALdGVybWl1cy5jb20="
