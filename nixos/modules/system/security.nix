26c5820c3262d0f8efe751f1663ce40eb7bf0df0
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-05 20:26:28 +0200

    added debugging

--- nixos/modules/system/security.nix
+++ nixos/modules/system/security.nix
@@ -5,6 +5,10 @@
   security.pam.services.sudo.sshAgentAuth = true;
 
   security.sudo.extraConfig = ''
-      Defaults env_keep += "SSH_AUTH_SOCK"
-    '';
+    Defaults env_keep += "SSH_AUTH_SOCK"
+  '';
+
+  security.pam.services.sudo.rules.auth.ssh_agent_auth.settings = {
+    debug = true;
+  };
 }
