7efa1465fd512287131926d53d6ba1c5655daf34
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-05 20:14:56 +0200

    added preserve sshauthsock

--- nixos/modules/system/security.nix
+++ nixos/modules/system/security.nix
@@ -3,4 +3,8 @@
 {
   security.pam.sshAgentAuth.enable = true;
   security.pam.services.sudo.sshAgentAuth = true;
+
+  security.sudo.extraConfig = ''
+      Defaults env_keep += "SSH_AUTH_SOCK"
+    '';
 }
