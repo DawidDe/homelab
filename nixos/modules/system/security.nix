8511da382fa36f7063bf75f6bbf3a36272acd79a
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-05 20:09:26 +0200

    changed security.nix

--- nixos/modules/system/security.nix
+++ nixos/modules/system/security.nix
@@ -2,8 +2,5 @@
 
 {
   security.pam.sshAgentAuth.enable = true;
-
-  security.sudo.extraConfig = ''
-    Defaults keepEnv += "SSH_AUTH_SOCK"
-  '';
+  security.pam.services.sudo.sshAgentAuth = true;
 }
