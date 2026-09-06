322f04a63d2b1bfe098a0164c4b176c37e5915a2
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-06 17:14:18 +0200

    added firewall rule for incus

--- nixos/modules/services/incus.nix
+++ nixos/modules/services/incus.nix
@@ -2,4 +2,9 @@
 
 {
   virtualisation.incus.enable = true;
+
+  networking.firewall.extraInputRules = ''
+    # Allow Incus access only from 192.168.178.67
+    iifname "eno1" ip saddr 192.168.178.67 tcp dport 8443 accept
+  ''
 }
