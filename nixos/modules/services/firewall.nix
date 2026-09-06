19ba6c96ecc2ebbadab7ae98cb9fe19d63e773e4
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-06 17:13:16 +0200

    moving firewall rules to their respective services

--- nixos/modules/services/firewall.nix
+++ nixos/modules/services/firewall.nix
@@ -5,10 +5,5 @@
   networking.firewall = {
     enable = true;
     backend = "nftables";
-
-    extraInputRules = ''
-      # Allow SSH only from 192.168.178.67
-      iifname "eno1" ip saddr 192.168.178.67 tcp dport 22 accept
-    '';
   };
 }
--- nixos/modules/services/ssh.nix
+++ nixos/modules/services/ssh.nix
@@ -17,4 +17,9 @@
       MaxAuthTries = 3;
     };
   };
+
+  networking.firewall.extraInputRules = ''
+    # Allow SSH only from 192.168.178.67
+    iifname "eno1" ip saddr 192.168.178.67 tcp dport 22 accept
+  '';
 }
