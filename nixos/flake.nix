84ea5bb6d3dcfdc7a77565e7a4bae2b5a253c13d
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-07 16:49:45 +0200

    fixed paths in build job. added the sd image installer to flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -20,24 +20,30 @@
       heimdall = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
+
         modules = [
           disko.nixosModules.disko
           ./hosts/heimdall/configuration.nix
         ];
       };
+
       pi-image = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         specialArgs = { inherit inputs; };
 
         modules = [
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          nixos-hardware.nixosModules.raspberry-pi-4
           ./hosts/pi/image.nix
         ];
       };
+
       pi = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         specialArgs = { inherit inputs; };
 
         modules = [
+          nixos-hardware.nixosModules.raspberry-pi-4
           ./hosts/pi/configuration.nix
         ];
       };
