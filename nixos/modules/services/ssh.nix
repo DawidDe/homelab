cb76d062a1fa4356106bdb507006a92eb01eee52
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-08 21:04:04 +0200

    added pis interface

--- nixos/modules/services/ssh.nix
+++ nixos/modules/services/ssh.nix
@@ -20,6 +20,6 @@
 
   networking.firewall.extraInputRules = ''
     # Allow SSH only from 192.168.178.67
-    iifname "eno1" ip saddr 192.168.178.67 tcp dport 22 accept
+    iifname { "end0", "eno1" } ip saddr 192.168.178.67 tcp dport 22 accept
   '';
 }
