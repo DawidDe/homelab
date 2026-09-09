ca5b25ff2d3e10b014af3cec9f9efc10adab5aba
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-09 19:57:17 +0200

    removed folder creation because he doesnt store permanent files

--- nixos/modules/containers/ddns.nix
+++ nixos/modules/containers/ddns.nix
@@ -1,10 +1,6 @@
 { config, lib, pkgs, ...}:
 
 {
-  systemd.tmpfiles.rules = [
-    "d /opt/ddns 0700 ddns ddns -"
-  ];
-
   virtualisation.oci-containers.containers.ddns = {
     image = "docker.io/favonia/cloudflare-ddns:latest";
     extraOptions = [
