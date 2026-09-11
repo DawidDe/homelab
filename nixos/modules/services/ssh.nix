9c151422969e3fb205f2e0da6004e2fdfd3a3b69
Dawid Jacek Imbrzykowski <dawidimb@proton.me>
2026-09-11 14:07:28 +0200

    temporarely allowing ssh access from my router

--- nixos/modules/services/ssh.nix
+++ nixos/modules/services/ssh.nix
@@ -21,5 +21,6 @@
   networking.firewall.extraInputRules = ''
     # Allow SSH only from 192.168.178.67
     iifname { "end0", "eno1" } ip saddr 192.168.178.67 tcp dport 22 accept
+    iifname { "end0", "eno1" } ip saddr 192.168.178.1 tcp dport 22 accept
   '';
 }
