9cf262fc02af75d64f19f3b5dea1c76891a21ab2
Dawid J. Imbrzykowski <dawidimb@proton.me>
2026-09-24 15:44:38 +0200

    changed incus remote hostname

--- base/terraform/main.tf
+++ base/terraform/main.tf
@@ -1,10 +1,10 @@
 provider "incus" {
     generate_client_certificates = true
     accept_remote_certificate = true
-    default_remote = "heimdall"
+    default_remote = "thor"
 
     remote {
-        name = "heimdall"
+        name = "thor"
         address = "https://192.168.178.3:8443"
         token = ""
     }
